47a7925cd510797fae4c9df3b99e0ce9ce1f2801
Salim Afiune <afiune@chef.io>
2020-01-16 08:21:00 -0700

    fix:(chefspec) update tests to run
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,22 +7,9 @@
 require 'spec_helper'
 
 describe 'go-counter-cookbook::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
-
+  context 'When all attributes are default' do
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
+      stub_command("hab svc status afiune/go-counter").and_return("")
       expect { chef_run }.to_not raise_error
     end
   end
